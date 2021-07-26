ca49f074b2c88b18e79c04051ad7405d24eab0eb
NikolaySokolov <nikcsokolof@mail.ru>
2021-07-27 01:34:05 +0300

    add metrics from "Crowdsourcing the creation of image segmentation algorithms for connectomics"

--- testMetrics.py
+++ testMetrics.py
@@ -3,6 +3,7 @@ import cv2
 import copy
 import os
 import json
+import math
 
 import skimage.io as io
 
@@ -122,6 +123,81 @@ def Fscore(y_true, y_pred):
 	
 	return (2*precition*recall)/(precition+recall)
 
+
+def CrowdsourcingMetrics(y_true, y_pred):
+	y_true = np.asarray(y_true, np.bool)
+	y_pred = np.asarray(y_pred, np.bool)
+
+	y_true = np.asarray(y_true, np.int16)
+	y_pred = np.asarray(y_pred, np.int16)
+
+	y_true = y_true.ravel()
+	y_pred = y_pred.ravel()
+
+	n = len(y_true)
+	num_class = 1
+
+	pij_matrix = np.zeros((num_class + 1, num_class + 1), np.float64)
+
+	for i in range(len(y_true)):
+		pij_matrix[y_pred[i], y_true[i]] += 1
+
+	# print (pij_matrix)
+	# print (n, pij_matrix.sum(), pij_matrix.sum()/n)
+	pij_matrix = pij_matrix / n  # pij_matrix.sum()
+	# print (pij_matrix)
+	# print (n, pij_matrix.sum())
+
+	s_i_arr = np.zeros(pij_matrix.shape[0], np.float64)
+	for i in range(0, pij_matrix.shape[0]):
+		for j in range(pij_matrix.shape[1]):
+			s_i_arr[i] += pij_matrix[i][j]
+
+	t_j_arr = np.zeros(pij_matrix.shape[1], np.float64)
+	for j in range(0, pij_matrix.shape[1]):
+		for i in range(0, pij_matrix.shape[0]):
+			t_j_arr[j] += pij_matrix[i][j]
+
+	sqr_t_sum = (t_j_arr ** 2).sum()
+	sqr_s_sum = (s_i_arr ** 2).sum()
+	sqr_pij_sum = (pij_matrix ** 2).sum()
+
+	# print(s_i_arr, t_j_arr)
+	# print(sqr_t_sum, sqr_s_sum)
+	# print(pij_matrix**2)
+	# print(sqr_pij_sum, "***")
+
+	Vrand_split = sqr_pij_sum / sqr_t_sum
+	Vrand_merge = sqr_pij_sum / sqr_s_sum
+
+	Rand_Fscore = 2.0 * sqr_pij_sum / (sqr_t_sum + sqr_s_sum)
+
+	p_logp = 0
+	for i in range(0, pij_matrix.shape[0]):
+		for j in range(0, pij_matrix.shape[1]):
+			if pij_matrix[i, j] != 0:
+				p_logp += pij_matrix[i, j] * math.log(pij_matrix[i, j])
+	s_logs = 0
+	for s_i in s_i_arr[:]:
+		if s_i != 0:
+			s_logs -= s_i * math.log(s_i)
+	t_logt = 0
+	for t_j in t_j_arr[:]:
+		if t_j != 0:
+			t_logt -= t_j * math.log(t_j)
+
+	I = p_logp + s_logs + t_logt
+
+	Vinfo_split = I / (s_logs)
+	Vinfo_merge = I / (t_logt)
+
+	InformationTheoreticFscore = 2.0 * I / (s_logs + t_logt)
+
+	print("Vrand_split", "Vrand_merge", "Rand_Fscore", "Vinfo_split", "Vinfo_merge", "InformationTheoreticFscore")
+	print(Vrand_split, Vrand_merge, Rand_Fscore, Vinfo_split, Vinfo_merge, InformationTheoreticFscore)
+	return [Vrand_split, Vrand_merge, Rand_Fscore, Vinfo_split, Vinfo_merge, InformationTheoreticFscore]
+
+
 mask_name_label_list = ["mitochondria", "PSD", "vesicles", "axon", "boundaries", "mitochondrial boundaries"]
 
 name_img = "training0003.png"
@@ -141,7 +217,7 @@ for i in range(len(list_CNN_num_class)):
 	print(result_CNN_json_name[i])
 
 	print("class\metrics", "jaccard", "dice", "RI", "Accuracy", "AdaptedRandError", "Fscore")
-	json_list = [["class\metrics", "jaccard", "dice", "RI", "Accuracy", "AdaptedRandError", "Fscore"]]
+	json_list = [["class\metrics", "jaccard", "dice", "RI", "Accuracy", "AdaptedRandError", "Fscore", "Vrand_split", "Vrand_merge", "Rand_Fscore", "Vinfo_split", "Vinfo_merge", "InformationTheoreticFscore"]]
 	for index_label_name in range(num_class):
 		original_name = os.path.join("data/original data/", mask_name_label_list[index_label_name], name_img)
 		etal = io.imread(original_name, as_gray=True)
@@ -202,8 +278,9 @@ for i in range(len(list_CNN_num_class)):
 		res4 = Accuracy(y_true, y_pred)
 		res5 = Fscore(y_true, y_pred) #Adapted Rand Error
 
+		Vrand_split, Vrand_merge, Rand_Fscore, Vinfo_split, Vinfo_merge, InformationTheoreticFscore = CrowdsourcingMetrics(y_true, y_pred)
 		print(mask_name_label_list[index_label_name], rez, rez2, res3, res4, 1-res5, res5)
-		json_list.append([mask_name_label_list[index_label_name], rez, rez2, res3, res4, 1-res5, res5])
+		json_list.append([mask_name_label_list[index_label_name], rez, rez2, res3, res4, 1-res5, res5, Vrand_split, Vrand_merge, Rand_Fscore, Vinfo_split, Vinfo_merge, InformationTheoreticFscore])
 
 		cv2.waitKey(0)
 		cv2.destroyAllWindows()
