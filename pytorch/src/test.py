added testing with any activation fixed calculation of multi-losses in trainig created a tool for viewing train data for an epoch

pytorch/src/test.py
@@ -65,12 +65,13 @@ def saveResultMask(save_path, npyfile, namelist, num_class = 2 , classnames = ['
 
             if (os.path.isfile(os.path.join(out_dir, "predict_" + namelist[i]))):
                 os.remove(os.path.join(out_dir, "predict_" + namelist[i]))
-            io.imsave(os.path.join(out_dir, "predict_" + namelist[i]), item[:,:,class_index])
+            io.imsave(os.path.join(out_dir, "predict_" + namelist[i]), item[:,:,class_index], check_contrast=False)
 
 def predictModel(model, data, device, last_activation, eps = EPSILON):
     result = []
     model.eval()
     with torch.no_grad():
+        print(last_activation)
         time.sleep(0.2)  # чтобы tqdm не печатал вперед print
         tqdm_test_loop = tqdm.tqdm(data, file=sys.stdout, desc="Test", colour="GREEN")
         for epoch_valid_iteration, inputs in enumerate(tqdm_test_loop):
@@ -83,7 +84,7 @@ def predictModel(model, data, device, last_activation, eps = EPSILON):
     return np.array(result)
 
 
-def test_tiled(model_path, num_class, save_mask_dir, dataset={'filenames': None, "filepath": "data/test", "classnames": ['0', '1', '2', '3', '4', '5', '6']},
+def test_tiled(model_path, num_class, save_mask_dir, last_activation = None, dataset={'filenames': None, "filepath": "data/test", "classnames": ['0', '1', '2', '3', '4', '5', '6']},
                tiled_data={"size":256, "overlap":64, "unique_area":0}, save_dir = None):
     filenames = dataset["filenames"]
     filepath = dataset["filepath"]
@@ -97,8 +98,8 @@ def test_tiled(model_path, num_class, save_mask_dir, dataset={'filenames': None,
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+    last_activation = "sigmoid_activation" if last_activation is None else last_activation
     model.to(device)
-    last_activation = 'sigmoid_activation'
 
     for i, img_name in enumerate(filenames):
         print(i + 1, "image is ", len(filenames))

pytorch/src/train.py
@@ -195,9 +195,10 @@ def fitModel(my_data_generator,
                     torch.save(model, modelName + '.pt')
                     print(f"Loss of the model has decreased. {minimum_validation_error} to {val_history_losses[-1]}. model {modelName} was saving", flush=True)
                     minimum_validation_error = val_history_losses[-1]
+                else:
+                    print(f"best result loss: {minimum_validation_error}, on epoch: {model_saving_epoch}")
 
                 validation_work_time.append(tqdm_valid_loop.format_dict['elapsed'])
-
         else:
             if use_validation and not len(my_data_generator.gen_valid) > 0:
                 print("WARNING!!! Validation dataset error ! I use train data !")
@@ -208,6 +209,8 @@ def fitModel(my_data_generator,
                 torch.save(model, modelName + '.pt')
                 print(f"Loss of the model has decreased. {minimum_validation_error} to {history_losses[-1]}. model {modelName} was saving", flush=True)
                 minimum_validation_error = history_losses[-1]
+            else:
+                print(f"best result loss: {minimum_validation_error}, on epoch: {model_saving_epoch}")
 
         my_data_generator.on_epoch_end()
 